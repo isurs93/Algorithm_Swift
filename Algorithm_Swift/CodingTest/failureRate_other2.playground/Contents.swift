02ee1ecff4f344bff4830916096a9a0a6c826b3b
isurs93 <loadofcastle@gmail.com>
2020-07-31 19:53:45 +0900

    드디어 풀었다.... failure_other2 가 정답임...

--- Algorithm_Swift/CodingTest/failureRate_other2.playground/Contents.swift
+++ Algorithm_Swift/CodingTest/failureRate_other2.playground/Contents.swift
@@ -2,32 +2,37 @@ import Cocoa
 
 func solution(_ N:Int, _ stages:[Int]) -> [Int] {
     var eachFailure = Dictionary<Int, Double>()
-    var stageList = stages.sorted().reversed()
+    var stageList = stages.sorted()
+    
+    var beforeIndex = 0
+    
     for stage in 1...N {
         if !stageList.contains(stage) {
             eachFailure.updateValue(0.0, forKey: stage)
-            beforeIndex = stageList.lastIndex(of: stage) ?? 0
             continue
         }
         
         var notClear:Double = 0
         var clear:Double = 0
-        for each in stageList {
-            if each == stage {
+        var stageLastIndex = 0
+        for index in beforeIndex..<stageList.count {
+            if stageList[index] == stage {
                 notClear += 1
+                stageLastIndex += 1
             }
-            else if each > stage { clear += 1 }
+            else if stageList[index] > stage { clear += 1 }
         }
         
         let value = notClear != 0 ? notClear/(clear+notClear) : 0
         eachFailure.updateValue(value, forKey: stage)
+        
+        beforeIndex = stageLastIndex
     }
 
     return eachFailure.sorted(by: {
         if $0.value == $1.value { return $0.key < $1.key }
         else { return $0.value > $1.value }
     }).map({$0.key})
-
 }
 
 print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
